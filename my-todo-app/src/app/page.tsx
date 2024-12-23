f785b2cd87105054633fcd4090cab98bf948b4f5
MikeSedaya <sedayamichael82@gmail.com>
2024-12-23 19:20:47 +0800

    Added basic function of like add, remove and complete todo app

--- my-todo-app/src/app/page.tsx
+++ my-todo-app/src/app/page.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { TodoInterface } from "./types/ITodo";
 
 export default function Home() {
+  const [todos, setTodos] = useState<TodoInterface[]>([
+    { id: 1, title: "Todo 1", completed: false },
+  ]);
+  const [todoText, setTodoText] = useState<string>("");
+
+  function addTodo() {
+    setTodos([...todos, { id: Date.now(), title: todoText, completed: false }]);
+    setTodoText("");
+  }
+
+  function toggleCompleted(id: number) {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  }
+
+  function removeTodo(id: number) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return <div>Home</div>;
 }
